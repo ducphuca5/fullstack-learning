4b67b0e44e15f5b0537b5d8486b03a1247767ad3
Peter Tran <ducphuca5@yahoo.com>
2024-05-09 17:38:11 +1000

    homework w13

---
+++ week-13/todo-list.js
@@ -0,0 +1,5 @@
+let newTodoInput = document.querySelector('[data-component="new-todo-input"]');
+let addTodoButton = document.querySelector('[data-component="add-todo"]');
+let todoItemsContainer = document.querySelector(
+  '[data-component="todo-list-items"]'
+);
